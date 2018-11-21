f972091877eba45a576ef5488fa0af0f7951e3dc
Very-Red <bmk5cc@virginia.edu>
2018-11-21 16:51:48 -0500

    add with validation works

--- students/views.py
+++ students/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect
+from django.db import connections,transaction
+
 
 # Create your views here.
 from django.shortcuts import render
@@ -34,52 +36,54 @@ def add_class(request):
                                     FROM students_course
                                     WHERE name == %s''', [str(class_name)])
 
-        # if not class_id:
-        #     return render(request, 'students/index.html', {
-        #         'error': 'Class does not exist.'
-        #     })
+        if not class_id:
+            return render(request, 'students/index.html', {
+                'error': 'Class does not exist.'
+            })
         for item in class_id:
             class_id_int = item.id
         for item in user_id:
             user_id_int = item.id
-        # my_prereqs = Student.objects.raw('''SELECT students_course_prereqs.to_course_id as id
-        #                                      FROM students_course
-        #                                      INNER JOIN students_student_courses
-        #                                      ON students_course.id = students_student_courses.course_id
-        #                                      INNER JOIN students_student
-        #                                      ON students_student_courses.student_id = students_student.id
-        #                                      INNER JOIN students_course_prereqs
-        #                                      ON students_course_prereqs.from_course_id = students_course.id
-        #                                      WHERE students_student.name == %s
-        #                                      AND students_course_prereqs.from_course_id = %d''',
-        #                                     [str(username), int(class_id_int)])
-        # class_prereqs = Course.objects.raw('''SELECT students_course_prereqs.to_course_id as id
-        #                                      FROM students_course
-        #                                      INNER JOIN students_course_prereqs
-        #                                      ON students_course_prereqs.from_course_id = students_course.id
-        #                                      WHERE students_course_prereqs.from_course_id = %d''',
-        #                                    [int(class_id_int)])
-        # print(my_prereqs)
-        # print(class_prereqs)
-        # my_prereqs_list = []
-        # print("@@@@@@@")
-        # for item in my_prereqs:
-        #     #my_prereqs_list.append(item.)
-        #     print(item.id)
-        # print(my_prereqs_list)
-        # class_prereqs_list = []
-        # for item in class_prereqs:
-        #     class_prereqs_list.append(item.name)
-        #
-        # print(class_prereqs_list)
-        # if not set(my_prereqs_list)==set(class_prereqs_list):
-        #     return render(request, 'students/index.html', {
-        #         'error': 'Prereqs not satisfied.'
-        #     })
-        Student.objects.raw('''INSERT INTO students_student_courses (student_id, course_id)
-                                        VALUES (%d, %d)''', [int(user_id_int), int(class_id_int)])
-        print(user_id_int, class_id_int)
-        print("success")
+        my_prereqs = Student.objects.raw('''SELECT students_course_prereqs.to_course_id as id
+                                             FROM students_course
+                                             INNER JOIN students_student_courses
+                                             ON students_course.id = students_student_courses.course_id
+                                             INNER JOIN students_student
+                                             ON students_student_courses.student_id = students_student.id
+                                             INNER JOIN students_course_prereqs
+                                             ON students_course_prereqs.from_course_id = students_course.id
+                                             WHERE students_student.name == %s
+                                             AND students_course_prereqs.from_course_id = %s''',
+                                            [str(username), class_id_int])
+        class_prereqs = Course.objects.raw('''SELECT students_course_prereqs.to_course_id as id
+                                             FROM students_course
+                                             INNER JOIN students_course_prereqs
+                                             ON students_course_prereqs.from_course_id = students_course.id
+                                             WHERE students_course_prereqs.from_course_id = %s''',
+                                           [class_id_int])
+        print(my_prereqs)
+        print(class_prereqs)
+        my_prereqs_list = []
+        print("@@@@@@@")
+        for item in my_prereqs:
+            my_prereqs_list.append(item.name)
+            print(item.name)
+
+        class_prereqs_list = []
+        for item in class_prereqs:
+            class_prereqs_list.append(item.name)
+            print(item.name)
+
+        if not set(my_prereqs_list)==set(class_prereqs_list):
+            return render(request, 'students/index.html', {
+                'error': 'Prereqs not satisfied.'
+            })
+        cursor = connections['default'].cursor()
+
+        cursor.execute('''INSERT INTO students_student_courses(student_id, course_id)
+                                        VALUES (%s, %s)''', [user_id_int, class_id_int])
+
+
     return redirect('index',)
 
 
